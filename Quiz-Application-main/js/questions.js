eb83ce20ee1d3e459a41a28dff344dcda229de65
Venkatchinthakindi <143073525+Venkatchinthakindi@users.noreply.github.com>
2023-10-03 17:11:22 +0530

    Add files via upload

---
+++ Quiz-Application-main/js/questions.js
@@ -0,0 +1,167 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Who is the father of HTML?",
+    answer: "Tim Berners-Lee",
+    options: [
+      "Rasmus Lerdorf",
+      "Tim Berners-Lee",
+      "Brendan Eich",
+      "Sergery Brin"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which of the following is used to read an HTML page and render it?",
+    answer: "web browser",
+    options: [
+      "web application",
+      "web network",
+      "web server",
+      "web browser"
+    ]
+  },
+  {
+    numb: 8,
+    question: "In which part of the HTML metadata is contained?",
+    answer: "head tag",
+    options: [
+      "head tag",
+      "title tag",
+      "html tag",
+      "body tag"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Which program do you need to write HTML?",
+    answer: "Any text editor",
+    options: [
+      " A graphics program",
+      "Any text editor",
+      "HTML -development suite 4",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 10,
+    question: " What does the <br> tag add to your webpage?",
+    answer: "line break",
+    options: [
+      "long break",
+      "para break",
+      "line break",
+      "word break"
+    ]
+  },
+  {
+    numb: 11,
+    question: " Which element is used for or styling HTML5 layout?",
+    answer: "CSS",
+    options: [
+      "CSS",
+      "jQuery",
+      "javascript",
+      "PHP"
+    ]
+  },
+  {
+    numb: 12,
+    question: "In HTML, tags that include both on and off tag are called",
+    answer: "container tag",
+    options: [
+      "commment tag",
+      "document tag",
+      "container tag",
+      "All of the above"
+    ]
+  },
+  {
+    numb: 13,
+    question: "When is the content of a table shown?",
+    answer: "After the table is loaded",
+    options: [
+      "Before the border loads",
+      "After the table is loaded",
+      "In pieces as it loads",
+      "None of the above"
+    ]
+  },
+  {
+    numb: 14,
+    question: "HTML is a subset of ___________",
+    answer: "SGML",
+    options: [
+      "SGMT",
+      "SGML",
+      "SGME",
+      "XHTML"
+    ]
+  },
+  {
+    numb: 15,
+    question: "What is the work of <address> element in HTML5?",
+    answer: "contains contact details for author",
+    options: [
+      "contains IP address",
+      "contains HOME address",
+      "contains IP URL",
+      "contains contact details for author"
+    ]
+  },
+];
